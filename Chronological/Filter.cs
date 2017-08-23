05c7146cda034a0d77eb70eb1d545d53693824bd
colethecoder <colethecoder@gmail.com>
2017-08-23 19:31:54 +0100

    Changed filters to accept values as double or datetime as string

--- Chronological/Filter.cs
+++ Chronological/Filter.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using System;
 
 namespace Chronological
 {
@@ -10,27 +11,57 @@ namespace Chronological
         private readonly List<Filter> _filters;
 
         private readonly Property _left;
-        private readonly Property _right;
-
+        private readonly string _rightAsString;
+        private readonly double? _rightAsDouble;
+        private readonly DateTime? _rightAsDateTime;
+        
         private readonly string _operator;
 
-        private Filter(bool singular, List<Filter> filters, Property left, Property right, string filterOperator)
+        private Filter(bool singular, List<Filter> filters, Property left, string right, string filterOperator)
         {
             _singlular = singular;
             _filters = filters;
             _left = left;
-            _right = right;
+            _rightAsString = right;
             _operator = filterOperator;
         }
 
-        public static Filter Equal(Property left, Property right)
+        private Filter(bool singular, List<Filter> filters, Property left, double right, string filterOperator)
+        {
+            _singlular = singular;
+            _filters = filters;
+            _left = left;
+            _rightAsDouble = right;
+            _operator = filterOperator;
+        }
+
+        private Filter(bool singular, List<Filter> filters, Property left, DateTime right, string filterOperator)
+        {
+            _singlular = singular;
+            _filters = filters;
+            _left = left;
+            _rightAsDateTime = right;
+            _operator = filterOperator;
+        }
+
+        //public static Filter Equal(Property left, Property right)
+        //{
+        //    return new Filter(true, null, left, right, "eq");
+        //}
+
+        public static Filter Equal(Property left, string right)
         {
             return new Filter(true, null, left, right, "eq");
         }
 
-        public static Filter Equal(Property left, string right)
+        public static Filter Equal(Property left, double right)
         {
-            return Equal(left, Property.Custom(right));
+            return new Filter(true, null, left, right, "eq");
+        }
+
+        public static Filter Equal(Property left, DateTime right)
+        {
+            return new Filter(true, null, left, right, "eq");
         }
 
         public static Filter And(Filter filter1, Filter filter2, params Filter[] additionalFilters)
@@ -52,9 +83,22 @@ namespace Chronological
 
         private JProperty ToSingularJProperty()
         {
+            if (_rightAsString != null)
+            {
+                return new JProperty(_operator, new JObject(
+                    _left.ToLeftJProperty(),
+                    new JProperty("right", _rightAsString)));
+            }
+            if (_rightAsDouble.HasValue)
+            {
+                return new JProperty(_operator, new JObject(
+                    _left.ToLeftJProperty(),
+                    new JProperty("right", _rightAsDouble.Value)));
+            }
+
             return new JProperty(_operator, new JObject(
-                _left.ToLeftJProperty(),
-                _right.ToRightJProperty()));
+                    _left.ToLeftJProperty(),
+                    new JProperty("right", new JObject(_rightAsDateTime.Value))));
         }
 
         private JProperty ToNestedJProperty()
