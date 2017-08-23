34c2b0bf25e1d09347e2d4a2b6d89cbc0f5387bf
colethecoder <colethecoder@gmail.com>
2017-08-23 23:03:55 +0100

    Events Query now has a EventQueryResults object result option

--- Chronological/EventsQuery.cs
+++ Chronological/EventsQuery.cs
@@ -5,6 +5,7 @@ using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Chronological.Events;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
@@ -153,5 +154,91 @@ namespace Chronological
 
             return responseContent;
         }
+
+        public async Task<EventQueryResults> ResultsToEventResultsAsync()
+        {
+            var inputPayload = ToJObject(_environment.AccessToken);
+            var webSocket = new ClientWebSocket();
+
+            var test = inputPayload.ToString();
+
+            Uri uri = new UriBuilder("wss", _environment.EnvironmentFqdn)
+            {
+                Path = "events",
+                Query = "api-version=2016-12-12"
+            }.Uri;
+
+            await webSocket.ConnectAsync(uri, CancellationToken.None);
+
+            byte[] inputPayloadBytes = Encoding.UTF8.GetBytes(inputPayload.ToString());
+            await webSocket.SendAsync(
+                new ArraySegment<byte>(inputPayloadBytes),
+                WebSocketMessageType.Text,
+                endOfMessage: true,
+                cancellationToken: CancellationToken.None);
+
+            EventQueryResults responseContent = null;
+            using (webSocket)
+            {
+                while (true)
+                {
+                    string message;
+                    using (var ms = new MemoryStream())
+                    {
+
+                        const int bufferSize = 16 * 1024;
+                        var temporaryBuffer = new byte[bufferSize];
+                        while (true)
+                        {
+                            WebSocketReceiveResult response = await webSocket.ReceiveAsync(
+                                new ArraySegment<byte>(temporaryBuffer),
+                                CancellationToken.None);
+
+                            ms.Write(temporaryBuffer, 0, response.Count);
+                            if (response.EndOfMessage)
+                            {
+                                break;
+                            }
+                        }
+
+                        ms.Position = 0;
+
+                        using (var sr = new StreamReader(ms))
+                        {
+                            message = sr.ReadToEnd();
+                        }
+                    }
+
+                    responseContent = JsonConvert.DeserializeObject<EventQueryResults>(message);
+
+                    //Todo error handling
+
+
+                    if (Math.Abs(responseContent.PercentCompleted - 100d) < 0.01)
+                    {
+                        break;
+                    }
+                }
+
+                if (webSocket.State == WebSocketState.Open)
+                {
+                    await webSocket.CloseAsync(
+                        WebSocketCloseStatus.NormalClosure,
+                        "CompletedByClient",
+                        CancellationToken.None);
+                }
+            }
+
+            return responseContent;
+        }
+
+        //public async Task<dynamic> ResultsToDynamic()
+        //{
+        //    var results = await ResultsToJObjectAsync();
+
+        //    throw new NotImplementedException();
+
+
+        //}
     }
 }
