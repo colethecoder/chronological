43346d9f2444ea522d2abc23400d58abf65054a9
colethecoder <colethecoder@gmail.com>
2017-08-25 22:30:06 +0100

    Custom JsonConverter added for Aggregates

---
+++ Chronological/QueryResults/Aggregates/AggregateQueryMeasure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Chronological.QueryResults.Aggregates
+{
+    [JsonConverter(typeof(AggregateQueryMeasureJsonConverter))]
+    public class AggregateQueryMeasure
+    {
+        public List<AggregateQueryMeasure> Measures { get; set; }
+
+        public double Measure { get; set; }
+    }
+}
--- Chronological/QueryResults/Aggregates/AggregateResult.cs
+++ Chronological/QueryResults/Aggregates/AggregateResult.cs
@@ -9,6 +9,6 @@ namespace Chronological.QueryResults.Aggregates
         public List<string> Dimension { get; set; }
         public List<AggregateResult> Aggregates { get; set; }
 
-        public List<List<double>> Measures { get; set; }
+        public List<List<AggregateQueryMeasure>> Measures { get; set; }
     }
 }
