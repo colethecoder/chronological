eb65ab55cc473c0b9ea98d1684e7e291ab8569aa
colethecoder <colethecoder@gmail.com>
2017-08-25 22:23:02 +0100

    Aggregate,UniqueValues added

--- Chronological/Dimension.cs
+++ Chronological/Dimension.cs
@@ -7,6 +7,7 @@ namespace Chronological
         private readonly string _dimensionType;
         private readonly Property _property;
         private readonly Breaks _breaks;
+        private readonly Limit _limit;
 
         private Dimension(string dimensionType, Property property, Breaks breaks)
         {
@@ -15,18 +16,38 @@ namespace Chronological
             _breaks = breaks;
         }
 
+        private Dimension(string dimensionType, Property property, Limit limit)
+        {
+            _dimensionType = dimensionType;
+            _property = property;
+            _limit = limit;
+        }
+
         public static Dimension DateHistogram(Property property, Breaks breaks)
         {
             return new Dimension("dateHistogram", property, breaks);
         }
 
+        public static Dimension UniqueValues(Property property, Limit limit)
+        {
+            return new Dimension("uniqueValues", property, limit);
+        }
+
         internal JProperty ToJProperty()
         {
+            if (_dimensionType == "uniqueValues")
+            {
+                return new JProperty("dimension", new JObject(
+                    new JProperty(_dimensionType, new JObject(
+                        _property.ToInputJProperty(),
+                        _limit.ToJProperty()
+                    ))));
+            }
             return new JProperty("dimension", new JObject(
-                new JProperty(_dimensionType, new JObject(
-                    _property.ToInputJProperty(),
-                    _breaks.ToJProperty()
-                ))));
+                    new JProperty(_dimensionType, new JObject(
+                        _property.ToInputJProperty(),
+                        _breaks.ToJProperty()
+                    ))));
         }
     }
 }
